79e6f2c5007c15508344326f5979253a8a42bd84
Jeon Hyeonung <118552216+RatelXD@users.noreply.github.com>
2026-07-17 00:11:20 +0900

    로컬 채팅 정적 파일 제공 수정
    로컬 Compose 채팅 화면의 정적 파일 404를 수정합니다.

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,6 +2,7 @@
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 from config.health import liveness, readiness
@@ -18,4 +19,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
